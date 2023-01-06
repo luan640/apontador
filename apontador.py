5edc2500ce73ca167627202d1a0f21323c998a0b
luan <luan640@gmail.com>
2023-01-06 12:24:24 -0300

    versao2

--- apontador.py
+++ apontador.py
@@ -13,6 +13,8 @@ from selenium.webdriver.support.ui import WebDriverWait
 from sys import exit
 import gspread
 from selenium.webdriver.common.action_chains import ActionChains
+from gspread_dataframe import get_as_dataframe
+
 
 #link1 = "http://192.168.3.141/"
 link1 = 'http://devcemag.innovaro.com.br:81/sistema'
@@ -56,9 +58,9 @@ time.sleep(3)
 ########### ACESSANDO PLANILHAS ###########
 
 sheet = 'Apontamento automático'
-worksheet1 = 'Usinagem'
-worksheet2 = 'Estamparia'
-worksheet3 = 'Apontadas'
+worksheet1 = 'Serra'
+#worksheet2 = 'Estamparia'
+#worksheet3 = 'Apontadas'
 
 filename = "service_account.json"
 
@@ -66,39 +68,39 @@ sa = gspread.service_account(filename)
 sh = sa.open(sheet)
 
 wks1 = sh.worksheet(worksheet1)
-wks2 = sh.worksheet(worksheet2)
-wks3 = sh.worksheet(worksheet3)
+#wks2 = sh.worksheet(worksheet2)
+#wks3 = sh.worksheet(worksheet3)
 
-usinagem = wks1.get_all_records()
-estamparia = wks2.get_all_records()
-apontadas = wks3.get_all_records()
+serra = wks1.get_all_records()
+#estamparia = wks2.get_all_records()
+#apontadas = wks3.get_all_records()
 
-usinagem = pd.DataFrame(usinagem)
-estamparia = pd.DataFrame(estamparia)
-apontadas = pd.DataFrame(apontadas)
+serra = pd.DataFrame(serra)
+#estamparia = pd.DataFrame(estamparia)
+#apontadas = pd.DataFrame(apontadas)
 
 ########### Tratando planilhas ###########
 
 #ajustando datas
-for i in range(len(usinagem)):
+for i in range(len(serra)):
     try:
-        if usinagem['Data'][i] == "":
-            usinagem['Data'][i] = usinagem['Data'][i-1]
+        if serra['Data'][i] == "":
+            serra['Data'][i] = serra['Data'][i-1]
     except:
         pass
 
 i = None
 
 #filtrando pecas que não foram apontadas
-usinagem_filter = usinagem.loc[(usinagem.Status) == '']
+serra_filter = serra.loc[(serra.Status) == '']
 
 #inserindo 0 antes do código da peca
-usinagem_filter['Peca'] = usinagem_filter['Peca'].astype(str)
+serra_filter['Peca'] = serra_filter['Peca'].astype(str)
 
-for i in range(len(usinagem)):
+for i in range(len(serra)):
     try:
-        if len(usinagem_filter['Peca'][i]) == 5:
-            usinagem_filter['Peca'][i] = "0" + usinagem_filter['Peca'][i] 
+        if len(serra_filter['Peca'][i]) == 5:
+            serra_filter['Peca'][i] = "0" + serra_filter['Peca'][i] 
     except:
         pass
 
@@ -111,81 +113,103 @@ data_realizado = data_realizado.strftime('%d/%m/%Y')
 
 ########### PREENCHIMENTO ###########
 
-def preenchimento(peca,data,qtde,i,wks1,pessoa,c):
-
-    #mudando iframe
-    iframe1 = WebDriverWait(nav, 10).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[4]/div/div[2]/iframe')))
-    nav.switch_to.frame(iframe1)
-
-    #Insert
-    WebDriverWait(nav, 2).until(EC.element_to_be_clickable((By.XPATH, '/html/body/table/tbody/tr[1]/td/div/form/table/thead/tr[1]/td[1]/table/tbody/tr/td[2]/table/tbody/tr/td[2]/div'))).click()
-    time.sleep(2)
-
-    #classe
-    WebDriverWait(nav, 10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/table/tbody/tr[1]/td/div/form/table/tbody/tr[1]/td[1]/table/tbody/tr[" + str(c) + "]/td[3]/div/input"))).send_keys("Produção por máquina")
-    time.sleep(2)
-
-    #data
-    WebDriverWait(nav, 10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/table/tbody/tr[1]/td/div/form/table/tbody/tr[1]/td[1]/table/tbody/tr[" + str(c) + "]/td[5]/div/div"))).click()
-    time.sleep(2)
-    WebDriverWait(nav, 10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/table/tbody/tr[1]/td/div/form/table/tbody/tr[1]/td[1]/table/tbody/tr[" + str(c) + "]/td[5]/div/input"))).send_keys(data)
-
-    #pessoa
-    WebDriverWait(nav, 2).until(EC.element_to_be_clickable((By.XPATH, "/html/body/table/tbody/tr[1]/td/div/form/table/tbody/tr[1]/td[1]/table/tbody/tr[" + str(c) + "]/td[8]/div/div"))).click()
-    time.sleep(2)
-    WebDriverWait(nav, 2).until(EC.element_to_be_clickable((By.XPATH, "/html/body/table/tbody/tr[1]/td/div/form/table/tbody/tr[1]/td[1]/table/tbody/tr[" + str(c) + "]/td[8]/div/input"))).send_keys(pessoa)
-
-    #peça
-    WebDriverWait(nav, 2).until(EC.element_to_be_clickable((By.XPATH, "/html/body/table/tbody/tr[1]/td/div/form/table/tbody/tr[1]/td[1]/table/tbody/tr[" + str(c) + "]/td[10]/div/div"))).click()
-    time.sleep(2)
-    WebDriverWait(nav, 2).until(EC.element_to_be_clickable((By.XPATH, "/html/body/table/tbody/tr[1]/td/div/form/table/tbody/tr[1]/td[1]/table/tbody/tr[" + str(c) + "]/td[10]/div/input"))).send_keys(peca)
-    
-    WebDriverWait(nav, 2).until(EC.element_to_be_clickable((By.XPATH, "/html/body/table/tbody/tr[1]/td/div/form/table/tbody/tr[1]/td[1]/table/tbody/tr[" + str(c) + "]/td[12]/div/div"))).click()
-    time.sleep(5)
-
-    #processo
-    WebDriverWait(nav, 2).until(EC.element_to_be_clickable((By.XPATH, "/html/body/table/tbody/tr[1]/td/div/form/table/tbody/tr[1]/td[1]/table/tbody/tr[" + str(c) + "]/td[12]/div/input"))).send_keys('S Usinagem')
-    WebDriverWait(nav, 2).until(EC.element_to_be_clickable((By.XPATH, "/html/body/table/tbody/tr[1]/td/div/form/table/tbody/tr[1]/td[1]/table/tbody/tr[" + str(c) + "]/td[18]/div/div"))).click()
-    time.sleep(2)
-
-    #quantidade
-    WebDriverWait(nav, 2).until(EC.element_to_be_clickable((By.XPATH, "/html/body/table/tbody/tr[1]/td/div/form/table/tbody/tr[1]/td[1]/table/tbody/tr[" + str(c) + "]/td[18]/div/div"))).click()
-    time.sleep(2)
-    WebDriverWait(nav, 10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/table/tbody/tr[1]/td/div/form/table/tbody/tr[1]/td[1]/table/tbody/tr[" + str(c) + "]/td[18]/div/input"))).send_keys(qtde)
-    time.sleep(5)
-
-    ActionChains(nav).key_down(Keys.CONTROL).send_keys('m').key_up(Keys.CONTROL).perform()
-    time.sleep(2)
-    ActionChains(nav).key_down(Keys.CONTROL).send_keys('m').key_up(Keys.CONTROL).perform()
-    time.sleep(2)
-    
-    #innovaro real
-    #WebDriverWait(nav, 10).until(EC.element_to_be_clickable((By.XPATH, '/html/body/table/tbody/tr[1]/td/div/form/table/thead/tr[1]/td[1]/table/tbody/tr/td[2]/table/tbody/tr/td[4]/div'))).click()
-    #time.sleep(2)
-
-    time.sleep(2)
-
-    # volta p janela principal (fora do iframe)
-    nav.switch_to.default_content()
-
-    try:
-        texto_erro = nav.find_element("xpath", '/html/body/div[10]/div[2]/table/tbody/tr[1]/td[2]/div/div/span[1]').text
-        WebDriverWait(nav, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="confirm"]'))).click()
-        wks1.update('D' + str(i+2), texto_erro)
-    except:
-        wks1.update('D' + str(i+2), 'Apontada!')
-
-
 c = 1
 
-for i in range(0,3):
+i = 0
+
+for i in range(len(serra)):
+
     try:
-        peca = usinagem_filter['Peca'][i]
-        qtde = str(usinagem_filter['Qtde'][i])
-        data = usinagem_filter['Data'][i]
+        peca = serra_filter['Peca'][i]
+        qtde = str(serra_filter['Qtde'][i])
+        data = serra_filter['Data'][i]
         pessoa = '4054'
         c = c+2
-        preenchimento(peca,data,qtde,i,wks1,pessoa,c)
+
+        try:
+            nav.switch_to.default_content()
+        except:
+            pass
+
+        #mudando iframe
+        iframe1 = WebDriverWait(nav, 10).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[4]/div/div[2]/iframe')))
+        nav.switch_to.frame(iframe1)
+
+        #Insert
+        WebDriverWait(nav, 2).until(EC.element_to_be_clickable((By.XPATH, '/html/body/table/tbody/tr[1]/td/div/form/table/thead/tr[1]/td[1]/table/tbody/tr/td[2]/table/tbody/tr/td[2]/div'))).click()
+        time.sleep(2)
+
+        #classe
+        try:
+            WebDriverWait(nav, 10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/table/tbody/tr[1]/td/div/form/table/tbody/tr[1]/td[1]/table/tbody/tr[" + str(c) + "]/td[3]/div/div"))).click()
+        except:
+            pass
+        
+        try:
+            WebDriverWait(nav, 10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/table/tbody/tr[1]/td/div/form/table/tbody/tr[1]/td[1]/table/tbody/tr[" + str(c) + "]/td[3]/div/input"))).send_keys("Produção por máquina")
+            time.sleep(2)
+        except:
+            pass
+        
+        #data
+        WebDriverWait(nav, 10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/table/tbody/tr[1]/td/div/form/table/tbody/tr[1]/td[1]/table/tbody/tr[" + str(c) + "]/td[5]/div/div"))).click()
+        time.sleep(2)
+        WebDriverWait(nav, 10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/table/tbody/tr[1]/td/div/form/table/tbody/tr[1]/td[1]/table/tbody/tr[" + str(c) + "]/td[5]/div/input"))).send_keys(data)
+
+        #pessoa
+        WebDriverWait(nav, 2).until(EC.element_to_be_clickable((By.XPATH, "/html/body/table/tbody/tr[1]/td/div/form/table/tbody/tr[1]/td[1]/table/tbody/tr[" + str(c) + "]/td[8]/div/div"))).click()
+        time.sleep(2)
+        WebDriverWait(nav, 2).until(EC.element_to_be_clickable((By.XPATH, "/html/body/table/tbody/tr[1]/td/div/form/table/tbody/tr[1]/td[1]/table/tbody/tr[" + str(c) + "]/td[8]/div/input"))).send_keys(pessoa)
+
+        #peça
+        WebDriverWait(nav, 2).until(EC.element_to_be_clickable((By.XPATH, "/html/body/table/tbody/tr[1]/td/div/form/table/tbody/tr[1]/td[1]/table/tbody/tr[" + str(c) + "]/td[10]/div/div"))).click()
+        time.sleep(2)
+        WebDriverWait(nav, 2).until(EC.element_to_be_clickable((By.XPATH, "/html/body/table/tbody/tr[1]/td/div/form/table/tbody/tr[1]/td[1]/table/tbody/tr[" + str(c) + "]/td[10]/div/input"))).send_keys(peca)
+        
+        WebDriverWait(nav, 2).until(EC.element_to_be_clickable((By.XPATH, "/html/body/table/tbody/tr[1]/td/div/form/table/tbody/tr[1]/td[1]/table/tbody/tr[" + str(c) + "]/td[12]/div/div"))).click()
+        time.sleep(5)
+
+        #processo
+        WebDriverWait(nav, 2).until(EC.element_to_be_clickable((By.XPATH, "/html/body/table/tbody/tr[1]/td/div/form/table/tbody/tr[1]/td[1]/table/tbody/tr[" + str(c) + "]/td[12]/div/input"))).send_keys('S C Serras')
+        WebDriverWait(nav, 2).until(EC.element_to_be_clickable((By.XPATH, "/html/body/table/tbody/tr[1]/td/div/form/table/tbody/tr[1]/td[1]/table/tbody/tr[" + str(c) + "]/td[18]/div/div"))).click()
+        time.sleep(2)
+
+        #quantidade
+        WebDriverWait(nav, 2).until(EC.element_to_be_clickable((By.XPATH, "/html/body/table/tbody/tr[1]/td/div/form/table/tbody/tr[1]/td[1]/table/tbody/tr[" + str(c) + "]/td[18]/div/div"))).click()
+        time.sleep(2)
+        WebDriverWait(nav, 10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/table/tbody/tr[1]/td/div/form/table/tbody/tr[1]/td[1]/table/tbody/tr[" + str(c) + "]/td[18]/div/input"))).send_keys(qtde)
+        time.sleep(5)
+
+        ActionChains(nav).key_down(Keys.CONTROL).send_keys('m').key_up(Keys.CONTROL).perform()
+        time.sleep(2)
+        ActionChains(nav).key_down(Keys.CONTROL).send_keys('m').key_up(Keys.CONTROL).perform()
+        time.sleep(6)
+        
+        #innovaro real
+        #WebDriverWait(nav, 10).until(EC.element_to_be_clickable((By.XPATH, '/html/body/table/tbody/tr[1]/td/div/form/table/thead/tr[1]/td[1]/table/tbody/tr/td[2]/table/tbody/tr/td[4]/div'))).click()
+        #time.sleep(2)
+
+        time.sleep(6)
+
+        try:
+
+            # volta p janela principal (fora do iframe)
+            nav.switch_to.default_content()
+            texto_erro = nav.find_element("xpath", '/html/body/div[10]/div[2]/table/tbody/tr[1]/td[2]/div/div/span[1]').text
+            WebDriverWait(nav, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="confirm"]'))).click()
+            wks1.update('D' + str(i+2), texto_erro)
+            
+            WebDriverWait(nav, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="bt_1892603865"]/table/tbody/tr/td[2]'))).click()
+            time.sleep(5)
+            WebDriverWait(nav, 10).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[8]/div[2]/div[14]/span[2]'))).click()
+            time.sleep(5)
+            WebDriverWait(nav, 10).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[3]/div/table/tbody/tr/td[1]/table/tbody/tr/td[4]/span/div'))).click()
+            time.sleep(5)
+
+            c = 1
+
+        except:
+            wks1.update('D' + str(i+2), 'Apontada!')
 
     except:
-        pass
+        c = 1
